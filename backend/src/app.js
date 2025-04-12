26701df6648abcc47bbc004b67562e8680edc8c1
Om rawat <omrawat.work@gmail.com>
2025-04-13 00:42:42 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,11 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://frolicking-fairy-9e5986.netlify.app/",
+];
+
 //uses
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -11,7 +16,14 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl, Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
