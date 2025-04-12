1f4df305336bc016f609046c61b2a00e33d7f357
Om rawat <omrawat.work@gmail.com>
2025-04-13 00:30:28 +0530

    deployment build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // ✅ Import BrowserRouter
-import './index.css';
-import store from './store/index.js'
-import { Provider } from "react-redux";  // ✅ Import Provider from react-redux
-import App from './App.jsx';
-import axios from 'axios';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
+import "./index.css";
+import store from "./store/index.js";
+import { Provider } from "react-redux"; // ✅ Import Provider from react-redux
+import App from "./App.jsx";
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3000/';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL; // Set the base URL for axios requests
 axios.defaults.withCredentials = true; // Enable sending cookies with requests
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>  {/* ✅ Wrap App with BrowserRouter */}
-      <Provider store ={store}>
-      <App />
+    <BrowserRouter>
+      {" "}
+      {/* ✅ Wrap App with BrowserRouter */}
+      <Provider store={store}>
+        <App />
       </Provider>
     </BrowserRouter>
   </StrictMode>,
